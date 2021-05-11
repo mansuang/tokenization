b87ed6999539f8de6b905e4d3659e528f0b679af
Mansuang Pawong <mansaung@hotmnail.com>
2021-05-11 21:24:42 +0700

    fix config

--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,7 @@
+require("dotenv").config({path: "./.env"});
 const path = require("path");
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const AccountIndex = 0;
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -8,6 +11,12 @@ module.exports = {
     develop: {
       port: 7545,
       host: "127.0.0.1",
+      network_id: 5777
+    },
+    ganache_local: {
+      provider: function() {
+        return new HDWalletProvider(process.env.MNEMONIC, "http://127.0.0.1:7545", AccountIndex);
+      },
       network_id: 5777
     }
   },
